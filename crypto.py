6c0462c65bccb72a65a8244db09f1285435d9384
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-08-23 02:59:21 -0700

    Delete crypto.py

--- crypto.py
+++
@@ -1,8 +0,0 @@
-import struct
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-import zstandard as ztd
-
-class KMSEnvelopeCompressor:
-    def __init__(self, kms_key_id: str, region_name: str = "us-east-1"):
-        self.kms_key_id = kms_key_id
-        self.region_name = region_name
